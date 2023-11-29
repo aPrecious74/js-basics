38ce16f45200fdb90caf730c037798124be13735
Precious Agyei <student.176918@worcesterschools.net>
2023-11-29 16:08:50 -0500

    changes.commit

--- CheckMyGrade/CheckMyGrade.js
+++ CheckMyGrade/CheckMyGrade.js
@@ -1,3 +1,5 @@
+document.querySelector('form').addEventListener('submit', checkGrade)
+
 console.log("Welcome");
 
 let grade= 81; 
@@ -7,3 +9,29 @@ const B=89;
 const C=79;
 const D=69;
 const F=60;
+
+function checkGrade(e) {
+    e.preventDefault() 
+
+    let myGrade = document.querySelector('input').value;
+    console.log('RUNNING ...' + myGrade);
+    if ( myGrade >= A){
+        console.log('You have an A');
+    }
+
+    if ( myGrade < A && myGrade >= B){
+        console.log('You have a B');
+    }
+
+    if (myGrade < B && myGrade >= C) {
+        console.log('You have a C');
+    }
+
+
+    if (myGrade < C && myGrade >= D) {
+        console.log('you have a D');
+    }
+    if (myGrade < D && myGrade >= F) {
+        console.log('you have an F');
+    }
+}
--- javascript practice/age-calc.js
+++ javascript practice/age-calc.js
@@ -1,26 +1,33 @@
-document.querySelector('form');
+document.querySelector('form').addEventListener('submit', checkAge)
 
 const license = 18;
 const jrop = 16;
 const rental = 25;
 
 
-function checkAge( myAge) {
+function checkAge(e) {
+    e.preventDefault()  
+    //prevents the form from loading
+
+    let myAge = document.querySelector('input').value;
+    let message = "";
+
     console.log('RUNNING ...' + myAge);
+
     if ( myAge < license && myAge >= jrop){
-        console.log('you can have a JrOp license');
+        message += 'you can have a JrOp license';
     }
 
     if ( myAge >= license){
-        console.log('you can have a regular license');
+        message +='you can have a regular license';
     }
 
     if (myAge >= rental) {
-        console.log('you can rent a car');
+        message +='you can rent a car';
     }
 
 
     if (myAge < jrop) {
-        console.log('you are too young to drive');
+        message +='you are too young to drive';
     }
 }
