4376fac430107a63050182650f2f0a11822fa38a
Precious Agyei <student.176918@worcesterschools.net>
2023-11-29 16:48:53 -0500

    changes.commit

--- CheckMyGrade/CheckMyGrade.js
+++ CheckMyGrade/CheckMyGrade.js
@@ -4,9 +4,11 @@ console.log("Welcome");
 
 const A=90;
 const B=89;
+const BO=80;
 const C=79;
+const CO=70;
 const D=69;
-const F=60;
+const DO=60;
 
 function checkGrade(e) {
     e.preventDefault() 
@@ -20,23 +22,21 @@ function checkGrade(e) {
         message += '<li>You have an A</li>';
     }
 
-    if ( myGrade < A && myGrade >= B){
+    if ( myGrade <= B && myGrade >C){
         message +='<li>You have a B</li>';
     }
 
-    if (myGrade < B && myGrade >= C) {
+    if (myGrade <=C && myGrade > D) {
         message +='<li>You have a C</li>';
     }
 
 
-    if (myGrade < C && myGrade >= D) {
-        message +='<li>you have a D</li>';
+    if (myGrade > DO && myGrade <=D) {
+        message +='<li>You have a D</li>';
     }
-    if (myGrade >F && myGrade <= D) {
-        message +='<li>you have a D</li>';
-    }
-    if (myGrade <=F) {
-        message +='<li>you have an F</li>';
+    if (myGrade <=DO) {
+        message +='<li>You have an F</li>';
+    
     }
     
  message += '</ul>'
